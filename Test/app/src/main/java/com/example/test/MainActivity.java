c8f037fff3a12374d16ee2e8b4c1b0d9bd194bbc
Leeyoujin <ujin2021@naver.com>
2019-12-24 23:30:38 +0900

    about layout

---
+++ Test/app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
